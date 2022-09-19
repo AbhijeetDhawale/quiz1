17f37f1d13befe4ce05b84b59d421d169aa7292d
AbhijeetDhawale <113893907+AbhijeetDhawale@users.noreply.github.com>
2022-09-19 15:34:12 +0530

    Add files via upload

---
+++ pro1/1.php
@@ -0,0 +1,55 @@
+
+<html>
+<head>
+<meta chrset="utf-8">
+<title></title>
+<style>
+body{
+ margin:0;
+ padding:0;
+ font-family: "Roboto", snas-serif;
+ }
+ 
+ header{
+  position: fixed;
+  background:#22242A;
+  padding:20px;
+  width:100%;
+  height:100px;
+  }
+ .left_area h3{
+  color:#fff;
+  margin:0;
+  padding-left:00px;
+  text-transform:uppercase;
+  font-size:60px;
+  font-width:160;
+  
+	   animation-name:ex1;
+	 animation-duration:10s;
+	 animation-iteration-count:infinite;
+	 }
+@keyframes ex1{
+	 from {color:#ff5733;}
+to {color:#FADO3F;}
+	 from {color:#FADO3F;}
+to {color:#1ABC9C;}
+
+	 from {color:#1ABC9C;}
+to {color:#A9CCF3;}
+ from {color:#A9CCF3;}
+to {color:#FF5733;}
+}
+  </style>
+</head>
+<body>
+<center>
+<header>
+<div class="left_area">
+<h3>ONLINE  <span>QUIZ</span></h3></center>
+</div>
+
+</header>
+
+</body>
+</html>
---
+++ pro1/a2.php
@@ -0,0 +1,57 @@
+<?php
+include '1.php'
+?>
+<html>
+<head>
+<meta chrset="utf-8">
+<title></title>
+<style>
+body{
+	 margin-left:0px;
+	 background:url(background1.jpeg) no-repeat;
+	 background-position:center;
+	 background-size:cover;
+	 height:100vh;
+	animation-name:ex4;
+	 animation-duration:4s;
+	 animation-iteration-count:infinite;
+	 }
+	 .a2{
+	 margin-bottom:400px;
+	font-size:48px;
+	 
+	}
+	.a2 span{
+	color:#F02121 ;}
+	.a2 a{
+	color:#CCEEFF;
+	
+	width:100%;
+	line-height:60px;
+	text-decoration:none;
+	padding-left:40px;
+	transition-property:background;
+}
+.a2 a:hover{
+	background:#198303;
+	
+}
+	 
+	 </style>
+	 </head>
+	 <body>
+	 <br><br><br><br><br><br><br><br><br>
+	 <br><br><br><br><br><br><br><br>
+
+<br><br><br>
+	 <div class="a2">
+  &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+&nbsp&nbsp&nbsp&nbsp<span><a href="admin.php"><span>ADMIN</span></a>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+ &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+ 
+&nbsp&nbsp&nbsp&nbsp<a href="login.php"><span>USER</span></a> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp
+ 
+</div>
+
+</body>
+</html>
